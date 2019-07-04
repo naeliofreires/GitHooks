c8fe1e8071e5812565ead8360181b4e4a8d35f8d
naeliofreires <naeliofreires@gmail.com>
2019-07-04 10:29:35 -0300

    estudo de hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    const watchID = navigator.geolocation.watchPosition(handlePosition);
+
+    return () => navigator.geolocation.clearWatch(watchID);
+  }, []);
+
+  function handlePosition({ coords }) {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude, longitude });
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        "https://api.github.com/users/naeliofreires/repos"
+      );
+      setRepositories(response.data);
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `${filtered.length} Favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Minhas Coordenadas</h1>
+      <b>Latitude: </b> {location.latitude} <br />
+      <b>Longitude: </b> {location.longitude}
+      <br />
+      <h1>Meus Repo no GitHub</h1>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.favorite && <span>[ * ]</span>}
+            {repo.name}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
-
-export default App;
